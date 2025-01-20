69c18d2f6b99125b7326c1fd2fc130a7b9e7fc00
KhoirulRifki <rifkikhoirulyk@gmail.com>
2025-01-20 11:13:06 +0700

    third update

--- MultipleFiles/o2_Lihat_anime.py
+++ MultipleFiles/o2_Lihat_anime.py
@@ -1,25 +1,3 @@
-# import tkinter as tk
-# from tkinter import messagebox
-# from pymongo import MongoClient
-
-# # Koneksi ke MongoDB
-# client = MongoClient('mongodb://localhost:27017/')
-# db = client['AnimeDB']
-
-# def lihat_anime():
-#     window = tk.Toplevel()
-#     window.title("Daftar Anime")
-
-#     # Mengambil data anime dari database
-#     anime_list = db.anime.find()
-
-#     for anime in anime_list:
-#         tk.Label(window, text=f"ID: {anime['_id']}, Judul: {anime['judul']}, Genre: {', '.join(anime['genre'])}, Tahun Rilis: {anime['tahun_rilis']}, Rating: {anime['rating']}, Sinopsis: {anime['sinopsis']}").pack()
-
-# # Jika Anda ingin menguji fungsi ini secara langsung
-# if __name__ == "__main__":
-#     lihat_anime()
-
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
